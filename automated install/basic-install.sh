bcc59812f9ae65dcba7da61433a93d14aa339d84
pi-simple-adblocker <77189139+pi-simple-adblocker@users.noreply.github.com>
2021-01-10 05:35:44 +0700

    Update basic-install.sh
    Update adminLTE Repo

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -60,7 +60,7 @@ coltable=/opt/pihole/COL_TABLE
 webroot="/var/www/html"
 
 # We store several other directories and
-webInterfaceGitUrl="https://github.com/pi-hole/AdminLTE.git"
+webInterfaceGitUrl="https://github.com/pi-simple-adblocker/AdminLTE.git"
 webInterfaceDir="${webroot}/admin"
 piholeGitUrl="https://github.com/pi-hole/pi-hole.git"
 PI_HOLE_LOCAL_REPO="/etc/.pihole"
